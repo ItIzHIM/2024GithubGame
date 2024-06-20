d49a9bdc967ec929e3a801fe09f899e80ee0ae4b
ItIzHIM <135070700+ItIzHIM@users.noreply.github.com>
2024-06-20 12:42:17 +1200

    simple grappling hook and less movement

--- 2024Game/Player.gd
+++ 2024Game/Player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody3D
 
-
 @onready var SPEED = 15
 @onready var ON_WALL = false
 const SIDE_SPEED = 15
@@ -13,11 +12,16 @@ const WALL_JUMP_VELOCITY = 8
 @onready var JUMP_COUNTER = 2
 @onready var WALL_JUMP_COUNTER = 1
 @onready var wall_sliding = false
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+@onready var Camera_Cast = $Neck/Camera3D/camera_cast
+@onready var grapple_line = $Neck/Camera3D/GrappleLine
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var neck := $Neck
 @onready var camera := $Neck/Camera3D
+@onready var camera_cast = $Neck/Camera3D/camera_cast
 
+# Variables for grappling hook
+var grappling = false
+var target_point = Vector3()
 
 func _unhandled_input(event: InputEvent):
 	if event is InputEventMouseButton:
@@ -31,72 +35,23 @@ func _unhandled_input(event: InputEvent):
 			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-70), deg_to_rad(70))
 			
 func _physics_process(delta):
-	if SPEED:
-		pass
-	if SPEED < 15:
-		SPEED = 15
-	# falling velocity increase 
-	if velocity.y < 0 and (velocity.x != 0 and velocity.z != 0 or (velocity.x > 0 or velocity.z > 0)) and SPEED < 30:
-		SPEED += 0.04
-	if velocity.y < 0 and (velocity.x != 0 and velocity.z != 0 or (velocity.x > 0 or velocity.z > 0)) and SPEED < 40 and SPEED >= 30:
-		SPEED += 0.02
-	if velocity.y < 0 and (velocity.x != 0 and velocity.z != 0 or (velocity.x > 0 or velocity.z > 0)) and SPEED < 47 and SPEED >= 40:
-		SPEED += 0.015
-	if velocity.y < 0 and (velocity.x != 0 and velocity.z != 0 or (velocity.x > 0 or velocity.z > 0)) and SPEED < 55 and SPEED >= 47:
-		SPEED += 0.01
-		
-	if is_on_floor() and SPEED > 15:
-		SPEED -= 0.6
-	if (velocity.x == 0 and velocity.z == 0 or (velocity.x == 0 or velocity.z == 0)) and SPEED > 15:
-		SPEED -= 0.9
-	if wall_sliding == true and SPEED < 55:
-		SPEED += 0.1
-	# Add the gravity.
-	if not is_on_floor() and !is_on_wall():
+	var space_state = get_world_3d().direct_space_state
+
+	# Add the gravity if not grappling
+	if not is_on_floor() and !is_on_wall() and !grappling:
 		velocity.y -= gravity * delta
-	
-	
-	
-	# hadle wall slide
-	if !is_on_floor() and is_on_wall():
-		if velocity.y == 0 or velocity.y < 0 and SPEED <= 17:
-			velocity.y -= 0.8 * gravity * delta
-			wall_sliding = true
-		if velocity.y == 0 or velocity.y < 0 and SPEED <= 25 and SPEED > 17:
-			velocity.y -= 0.3 * gravity * delta
-			wall_sliding = true
-		if velocity.y == 0 or velocity.y < 0 and SPEED < 70 and SPEED > 25:
-			velocity.y -= 0.1 * gravity * delta
-			wall_sliding = true
-		if velocity.y > gravity or velocity.y == gravity:
-			velocity.y -= 1.5 * gravity * delta 
-			wall_sliding = false
-		if velocity.y < gravity and velocity.y > 0:
-			velocity.y -= gravity * delta
-			wall_sliding = true
-	if wall_sliding == true and Input.is_action_just_pressed("ui_accept") and WALL_JUMP_COUNTER == 1:
-		velocity.y = WALL_JUMP_VELOCITY
-		wall_sliding = false
-		WALL_JUMP_COUNTER = 0
-	if !is_on_wall() and WALL_JUMP_COUNTER == 0:
-		WALL_JUMP_COUNTER = 1
-	if wall_sliding == true and !is_on_wall():
-		wall_sliding = false
-	
+
 	# Handle jump and double jump
-	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and JUMP_COUNTER == 2:
 		velocity.y = JUMP_VELOCITY
 		JUMP_COUNTER = 1
-	if JUMP_COUNTER == 1 and !is_on_floor() and Input.is_action_just_pressed("ui_accept") and wall_sliding == false:
+	if JUMP_COUNTER == 1 and !is_on_floor() and Input.is_action_just_pressed("ui_accept"):
 		velocity.y = DOUBLE_JUMP_VELOCITY
 		JUMP_COUNTER = 2
 	if is_on_floor() and !Input.is_action_just_pressed("ui_accept"):
 		JUMP_COUNTER = 2
-	
-	
+
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -108,17 +63,26 @@ func _physics_process(delta):
 	if direction and !is_on_floor():
 		velocity.x = direction.x * SIDE_SPEED
 		velocity.z = direction.z * SPEED
-		
+
+	# RayCast and grappling hook stuff
+	if Camera_Cast.is_colliding() and Input.is_action_pressed("grapple_action"):
+		if !grappling:
+			target_point = Camera_Cast.get_collision_point()
+			grappling = true
+
+	# Move towards the target point if grappling
+	if grappling and Input.is_action_pressed("grapple_action"):
+		var to_target = (target_point - global_transform.origin).normalized()
+		velocity = to_target * SPEED * 1.6
+		velocity.y -= gravity * delta
+
+		# Check if player has reached the target point
+		if global_transform.origin.distance_to(target_point) < 1.0:
+			grappling = false
+			velocity = Vector3.ZERO
+
+	# Reset grappling if the action is released
+	if grappling and !Input.is_action_pressed("grapple_action"):
+		grappling = false
+
 	move_and_slide()
-
-
-
-
-
-
-func _on_collision_shape_3d_2_child_entered_tree(node):
-	print("entered")
-
-
-func _on_collision_shape_3d_2_child_exiting_tree(node):
-	pass # Replace with function body.
